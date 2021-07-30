99d6acc6b16a10bc8fc191bc39a59710b5fbd3c1
Mohammed Zayan <zayanit@gmail.com>
2021-07-30 14:37:53 +0300

    Use os package

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,18 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
+# database_name = "trivia"
+# database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
+DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+# DB_USER = os.getenv('DB_USER', 'postgres')
+# DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'trivia')
+DB_PATH = 'postgresql://{}/{}'.format(
+    # DB_USER,
+    # DB_PASSWORD,
+    DB_HOST,
+    DB_NAME
+)
 
 db = SQLAlchemy()
 
@@ -14,7 +24,7 @@ setup_db(app)
 '''
 
 
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
